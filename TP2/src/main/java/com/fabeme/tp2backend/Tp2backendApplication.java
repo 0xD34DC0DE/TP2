53ec8adb33790603510aad655818a49146b455e9
emerickpoulin31415 <emerickpoulin31415@gmail.com>
2019-11-05 18:08:53 -0500

    Updated Project to use up to date versions

---
+++ TP2/src/main/java/com/fabeme/tp2backend/Tp2backendApplication.java
@@ -0,0 +1,13 @@
+package com.fabeme.tp2backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp2backendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Tp2backendApplication.class, args);
+    }
+
+}
--- emerick-rest-example/rest-tp2/src/main/java/com/techprimers/jpa/springjpahibernateexample/repository/AdminRepository.java
+++
@@ -1,9 +0,0 @@
-package com.techprimers.jpa.springjpahibernateexample.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.techprimers.jpa.springjpahibernateexample.model.Admin;
-
-public interface AdminRepository extends JpaRepository<Admin, Integer> {
-	public Admin findByEmail(String email);
-}
